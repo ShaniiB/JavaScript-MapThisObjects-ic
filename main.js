76f1578b97277876b87cebc57abe148a2766d57b
Shanythia Richards <shanythia.m.richards@gmail.com>
2021-09-04 22:24:33 -0700

    commiting

---
+++ main.js
@@ -0,0 +1,62 @@
+// alert("We're good!");
+
+// higher order functions
+// define array of names
+// const namesArray=["bob","mark", "kevin", "jody", "melissa"];
+
+// regular for loop
+// for (let x = 0; x <=10; x++)
+// { 
+//       // do some stuff  
+// }
+
+// forEach
+// let myCallback = function(el, index){
+
+//     // do stuff
+//     console.log(`${el} ${index}`);
+
+// }
+// // call foreach against my array
+// namesArray.forEach(myCallback);
+
+// function myOtherFn(elem)
+// {
+//     // do more stuff
+// }
+
+// namesArray.forEach(myOtherFn);
+
+// const namesArray=["bob","mark", "kevin", "jody", "melissa"];
+
+// // inline fucntion
+// // namesArray.forEach(
+// //     (mycrap, myidx) => {
+// //        console.log(`${mycrap}`);
+// //   }
+// // );  
+
+
+// // map - map each element to a new array
+// let allCapsArray = namesArray.map(
+//     (el, idx) => {
+//         return el.toUpperCase(); // return thew new mapped element
+//   }
+// ); // new printed array
+// console.log(`Original Array ${namesArray}`);
+// console.log(`New Array ${allCapsArray}`);
+
+// let origarray = [{name:"bob", age: 23},{name:"fred", age: 55}];
+
+// let justNamesfromObjects = origarray.map( 
+//     (el) => {
+//     return el.name; // justNames array will only have the name from each object in the new array
+//     }
+// );
+
+
+// reduce
+
+
+// filter
+
